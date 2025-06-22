b6a0ff5933795c77b29ac2824d22e850a827428c
ErikGazovic <erikgazovic879@gmail.com>
2025-06-22 15:36:31 +0200

    END

--- shopIndex.js
+++ shopIndex.js
@@ -43,8 +43,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+// ADD YOUR OWN CREDENCIALS !!! //
+
 const db = new pg.Client({
-    user: process.env.DB_USER,
+    user: process.env.DB_USER, 
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
