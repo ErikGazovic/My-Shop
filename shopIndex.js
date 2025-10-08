fe6a2069f5516a58ecbe52b3d9f5edbaec511044
ErikGazovic <155158251+ErikGazovic@users.noreply.github.com>
2025-10-08 10:52:25 +0200

    Update shopIndex.js

--- shopIndex.js
+++ shopIndex.js
@@ -775,8 +775,8 @@ app.get("/thank-you", (req, res) => {
 });
 
 app.post("/delete", async (req, res) => {
-    const productToDeleteID = req.body["product-id"];
-    const productToDeleteSIZE = req.body["product-size"];
+    const productToDeleteID = parseInt(req.body["product-id"]);
+    const productToDeleteSIZE = parseInt(req.body["product-size"]);
     const username = req.body.username;
     await pool.query("DELETE FROM cart WHERE cart_id IN (SELECT cart_id FROM cart WHERE product_id = $1 AND size = $2 LIMIT 1)", [productToDeleteID, productToDeleteSIZE]);
     res.redirect(`/${username}/my-cart`);
@@ -1301,6 +1301,7 @@ function checkRegisterPassword (password, reppeatedPassword) {
     }
     
 }
+
 
 
 
